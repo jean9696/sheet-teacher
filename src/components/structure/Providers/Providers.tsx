152d7b9578f61e944e3036944c8dd0988e570438
Jean Dessane <jean@habx.fr>
2020-04-14 19:58:13 +0200

    fix build

--- src/components/structure/Providers/Providers.tsx
+++ src/components/structure/Providers/Providers.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
-import { captureException } from '@habx/lib-client-reporting'
 import { ThemeProvider, Provider, DesignSystemThemePatch } from '@habx/ui-core'
 
 import FormHelperProvider from './FormHelperProvider'
@@ -21,7 +20,7 @@ class Providers extends React.PureComponent {
   }
 
   componentDidCatch(error, errorInfo) {
-    captureException(error, errorInfo)
+    console.error(error, errorInfo) // eslint-disable-line
   }
 
   render() {

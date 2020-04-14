b71e98364a1064b94f2924e4f1d4450d2e67b37e
Jean Dessane <jean@habx.fr>
2020-04-14 20:09:23 +0200

    remove not os libs

--- src/components/final-form/TextInput/index.ts
+++
@@ -1,8 +0,0 @@
-import { withFinalForm } from '@habx/lib-form-helper'
-import { TextInput, TextInputProps } from '@habx/ui-core'
-
-export default withFinalForm<string>({})(
-  TextInput as React.FunctionComponent<
-    TextInputProps & { onKeyPress?: Function }
-  >
-)
--- src/components/structure/Providers/FormHelperProvider.tsx
+++ src/components/structure/Providers/FormHelperProvider.tsx
@@ -1,26 +1,26 @@
 import * as React from 'react'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
-import { FormHelperProvider as DefaultFormHelperProvider } from '@habx/lib-form-helper'
-import { theme as UICoreTheme, useTheme } from '@habx/ui-core'
+//import { FormHelperProvider as DefaultFormHelperProvider } from '@habx/lib-form-helper'
+// import { theme as UICoreTheme, useTheme } from '@habx/ui-core'
 
-const ErrorContainer = styled.div<{ padding?: number }>`
-  color: ${UICoreTheme.color('warning')};
-  padding-top: ${({ padding }) => padding ?? 8}px;
-  padding-left: ${({ padding }) => padding ?? 8}px;
-  font-size: 12px;
-`
+// const ErrorContainer = styled.div<{ padding?: number }>`
+//   color: ${UICoreTheme.color('warning')};
+//   padding-top: ${({ padding }) => padding ?? 8}px;
+//   padding-left: ${({ padding }) => padding ?? 8}px;
+//   font-size: 12px;
+// `
 
 const FormHelperProvider: React.FunctionComponent<FormHelperProviderInterface> = ({
   children,
 }) => {
-  const theme = useTheme()
+  // const theme = useTheme()
   return (
-    <DefaultFormHelperProvider
-      errors={{ color: theme.colors.warning.base, component: ErrorContainer }}
+    <div
+    // errors={{ color: theme.colors.warning.base, component: ErrorContainer }}
     >
       {children}
-    </DefaultFormHelperProvider>
+    </div>
   )
 }
 

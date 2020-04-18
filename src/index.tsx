e2e5f5fe4cc444fea8e86f02b5abf832aae06f36
Jean Dessane <jean@habx.fr>
2020-04-18 16:44:23 +0200

    PWA

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,8 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
 import App from './App'
+import register from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+register()
